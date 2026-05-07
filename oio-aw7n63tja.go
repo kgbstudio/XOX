6208cfd489850aa5d1f49492530c5f973f30b78d
kgbstudio <kgbstudio7@gmail.com>
2026-05-07 22:44:12 +0300

    📝 via OIO.SO/aw7n63tja

---
+++ oio-aw7n63tja.go
@@ -0,0 +1,18 @@
+**Introduction to Go Programming**
+=====================================
+
+Go, also known as Golang, is a statically typed, compiled language developed by Google. It's designed to be simple, efficient, and easy to use. In this example, we'll create a simple "Hello, World!" program in Go.
+
+**Code**
+--------
+
+```go
+// hello.go
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
